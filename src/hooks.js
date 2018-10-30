08e3e050a988a988c3c664c1ed55a3d97bdbba05
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-10-30 00:36:29 -0200

    feat: added initial hooks implementation

---
+++ src/hooks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const hooks = map => Component => props => {
+  const hooked = {}
+
+  for (let key in map) {
+    if (map.hasOwnProperty(key)) {
+      hooked[key] = map[key](props)
+    }
+  }
+
+  return <Component {...props} {...hooked} />
+}
+
+export { hooks }
